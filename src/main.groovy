8f7eadded361d7ddd53edade280ebde0a475f2ad
Vinicius Kammradt <vinicius.k@informant.com.br>
2019-09-25 16:52:00 -0300

    :sparkles: Closures

--- src/main.groovy
+++ src/main.groovy
@@ -1,5 +1,36 @@
-def teste = '123adkkk'
-println teste[0..teste.indexOf('d')]
+def startsWithLetterK = { word -> 
+    word.startsWith('k')
+}
 
-def a = /aaa ${teste} bbb/
-println a
+assert startsWithLetterK('ak') == false
+assert startsWithLetterK.call('ka') == true
+
+def useMyClosure(closure, word) {
+    closure.call(word)
+}
+
+assert useMyClosure(startsWithLetterK, "ake") == false
+assert useMyClosure(startsWithLetterK, "kkk") == true
+
+
+def map = ['key111':'value1', 'key123':'value123']
+def map2 = ['key222':'value222', 'keyABC':'abcdef']
+def amazingList = [ map, map2 ]
+amazingList.each { json ->
+    println "This is the JSON: ${json}"
+}
+
+def intList = [1, 2, 32, 100, 123, 321, 1, 123]
+assert intList.find { item -> item == 123 } == 123
+assert intList.find { item -> item == 'abc' } == null
+
+assert intList.findAll { item -> item >= 100 } == [100, 123, 321, 123]
+
+assert intList.any { item -> item >= 300 } == true
+assert intList.any { item -> item >= 400 } == false
+
+assert intList.every { item -> item > 0 } == true
+assert intList.every { item -> item > 55 } == false
+
+assert intList.collect { item -> item * 10 } == [10, 20, 320, 1000, 1230, 3210, 10, 1230]
+assert intList == [1, 2, 32, 100, 123, 321, 1, 123]
